764ef3817886b71129829f971fdd65f725c7695f
“pantrypoints” <pantrypoints@gmail.com>
2026-09-20 20:46:25 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,10 @@
-// import adapter from '@sveltejs/adapter-auto';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
-
-
 import adapter from '@sveltejs/adapter-cloudflare';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
 		adapter: adapter()
 	}
 };
 
+export default config;
